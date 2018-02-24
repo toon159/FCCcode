ccbf67cb7372fd1079a81c5095cb718a8b6ef273
toon159 <toonbcc@gmail.com>
2018-02-24 10:10:44 +0700

    palindrome

--- CheckForPalindromes.js
+++ CheckForPalindromes.js
@@ -7,12 +7,17 @@ function palindrome (str) {
   newStr = newStr.toLowerCase();
   console.log(newStr);
   // palindrome
-
+  for (var i = 0; i < newStr.length / 2; i ++) {
+    // if any positions char is not equal opposite positions return false
+    if (newStr[i] != newStr[newStr.length - i - 1]) {
+      return false;
+    }
+  }
   return true;
 }
 console.log('this should be false');
-console.log(palindrome('oh'));
+console.log(palindrome('ohhhh'));
 console.log('this should be true');
-console.log(palindrome('W ow'));
+console.log(palindrome('W oow'));
 console.log('this should be true');
 console.log(palindrome('*wow^_'));
