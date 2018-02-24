9a2a16016b761ee58ec15a834a578b0ffccab02e
toon159 <toonbcc@gmail.com>
2018-02-24 10:04:21 +0700

    remove non alphanumeric char

--- CheckForPalindromes.js
+++ CheckForPalindromes.js
@@ -1,8 +1,18 @@
 
-function palindrome(str) {
-  str = "wow";
-  // Good luck!
+function palindrome (str) {
+  // remove non-alphanumeric cha
+  var newStr = str.replace(/[^a-z0-9]/ig, '');
+  console.log(newStr);
+  // to lowercase
+    newStr = newStr.toLowerCase();
+    console.log(newStr);
+  // palindrome
+
   return true;
 }
-console.log(palindrome("wow"));
-// palindrome is not defined?????
+console.log('this should be false');
+console.log(palindrome('oh'));
+console.log('this should be true');
+console.log(palindrome('W ow'));
+console.log('this should be true');
+console.log(palindrome('*wow^_'));
