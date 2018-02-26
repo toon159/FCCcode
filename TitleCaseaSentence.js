0cb08d44e5b365bc036ee64b0b91c283c76868cf
toon159 <toonbcc@gmail.com>
2018-02-27 02:51:59 +0700

    add comments

--- TitleCaseaSentence.js
+++ TitleCaseaSentence.js
@@ -3,25 +3,39 @@
 // For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 
 function titleCase(str) {
+  // add array and answer of type String
   var array = str.split(" ");
   var ans = "";
-  for (var i = 0; i < array.length; i++) {
 
+  for (var i = 0; i < array.length; i++) {
+// if word has more than 1 character
     if (array[i].length > 1) {
       var first;
       var anyLeft;
+
+      // cut first and any lest characters in the word
       first = array[i].substring(0,1);
       anyLeft = array[i].substring(1,array[i].length);
+
+      // toUpperCase toLowerCase
       first = first.toUpperCase();
       anyLeft = anyLeft.toLowerCase();
+
+      // concat first and anyLeft
       array[i] = first + anyLeft + " ";
+
+      // add them to answer
       ans += array[i];
       } else {
+        // only 1 character, so do toUpperCase
           array[i] = array[i].toUpperCase() + " ";
           ans += array[i];
       }
   }
+
+  //  trim the answer
   ans = ans.trim();
+
   return ans;
 }
 
