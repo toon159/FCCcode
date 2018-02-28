e317d47e1a387eaab98e906baa6a83426d1fd8c3
toon159 <toonbcc@gmail.com>
2018-02-28 13:21:33 +0700

    finish repeat a string challenge

--- basic-algorithm-scripting/repeat-a-string-repeat-a-string.js
+++ basic-algorithm-scripting/repeat-a-string-repeat-a-string.js
@@ -1,8 +1,16 @@
 // Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
 
 function repeatStringNumTimes(str, num) {
+
+  var answer = "";
   // repeat after me
-  return str;
+  if (num < 1) {
+    return "";
+  }
+  for (var i = 0; i < num; i++) {
+  answer += str;
+  }
+  return answer;
 }
 
 repeatStringNumTimes("abc", 3);
