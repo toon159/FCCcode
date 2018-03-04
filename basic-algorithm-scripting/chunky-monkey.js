c4c25f0ed03c2158e8ff1125ff7f869c09f666be
toon159 <toonbcc@gmail.com>
2018-03-04 23:07:39 +0700

    finish chunky-monkey challenge!

--- basic-algorithm-scripting/chunky-monkey.js
+++ basic-algorithm-scripting/chunky-monkey.js
@@ -2,8 +2,21 @@
 //  and returns them as a two-dimensional array.
 
 function chunkArrayInGroups(arr, size) {
-  // Break it up.
-  return arr;
+    // add new answer array
+    var answer = [];
+
+    for (var i = 0; i < arr.length; i += size) {
+        var newArr = [];
+        for (var j = 0; j < size; j++) {
+            // check if undefined
+            if (arr[i + j] != null) {
+                // push i i+1 i+2 and so on to array
+                newArr.push(arr[i + j]);
+            }
+        }
+        answer.push(newArr);
+    }
+  return answer;
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 3));
