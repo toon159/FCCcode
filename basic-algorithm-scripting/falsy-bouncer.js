5fbe7d3505b157274a231e8d27b64fb1baaaec8d
toon159 <toonbcc@gmail.com>
2018-03-05 03:08:50 +0700

    finish falsy-bouncer challenge!

--- basic-algorithm-scripting/falsy-bouncer.js
+++ basic-algorithm-scripting/falsy-bouncer.js
@@ -1,10 +1,16 @@
 // Remove all falsy values from an array.
 //
 // Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
-
-function bouncer(arr) {
-  // Don't show a false ID to this bouncer.
-  return arr;
+function isNotFalsy(value) {
+    return !(value === false || value === null || value === 0 || value === "" || value === undefined || Number.isNaN(value));
 }
 
-bouncer([7, "ate", "", false, 9]);
+function bouncer(arr) {
+    // Don't show a false ID to this bouncer.
+    arr = arr.filter(isNotFalsy);
+    return arr;
+}
+console.log(bouncer(["a", "b", "c"]));
+console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+console.log(bouncer([7, "ate", "", false, 9]));
+
